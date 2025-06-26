f145e89168cd17d8e8cd69406e1660dc0a94c026
HuntingSlate <adamwoy99@gmail.com>
2025-06-26 20:32:59 +0200

    batman

---
+++ src/main/java/com/tournamentmanager/backend/TournamentManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tournamentmanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentManagerBackendApplication.class, args);
+	}
+
+}
